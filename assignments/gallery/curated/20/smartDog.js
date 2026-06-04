35ad34221913b6e911384aaa73ac57a5c6da182e
gniziemazity <gniziemazity@gmail.com>
2026-06-04 09:47:16 +0300

    Cleanup

--- assignments/gallery/curated/20/smartDog.js
+++
@@ -1,9 +0,0 @@
-class SmartDog extends Dog {
-    constructor(name, age, furrColor, eyeColor) {
-        super(name, age, furrColor, eyeColor);
-    }
-    talk() {
-    console.log("Woof! I'm " + this.name + " 🐶");
-    return "Woof! I'm " + this.name + " 🐶";
-  }
-}
